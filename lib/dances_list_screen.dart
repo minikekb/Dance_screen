9f68eed19a1c63a8dd87bcbf223fd144015fba81
minikekb <minikekb@gmail.com>
2022-05-31 16:26:00 +0500

    Improved change DanceStyleWidget

--- lib/dances_list_screen.dart
+++ lib/dances_list_screen.dart
@@ -9,8 +9,7 @@ class DancesListScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SizedBox.expand(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
           children: const [
             Center(
               child: Text(
@@ -23,16 +22,65 @@ class DancesListScreen extends StatelessWidget {
               ),
             ),
             DanceStyleWidget(
-                url:
-                "https://thumbs.dreamstime.com/b/%D0%B6%D0%B8%D0%B2%D1%8F-bachata-%D0%BC%D1%83%D0%B7%D1%8B%D0%BA%D0%B0-%D0%BC%D0%BE-%D0%BE-%D1%8B%D1%85-%D0%BF%D0%B0%D1%80-%D1%82%D0%B0%D0%BD%D1%86%D1%83%D1%8F-%D0%B0%D1%82%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-78010801.jpg",
-                name: "Bachata"),
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%B6%D0%B8%D0%B2%D1%8F-bachata-%D0%BC%D1%83%D0%B7%D1%8B%D0%BA%D0%B0-%D0%BC%D0%BE-%D0%BE-%D1%8B%D1%85-%D0%BF%D0%B0%D1%80-%D1%82%D0%B0%D0%BD%D1%86%D1%83%D1%8F-%D0%B0%D1%82%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-78010801.jpg",
+              name: "Bachata",
+            ),
             DanceStyleWidget(
-                url:
-                "https://thumbs.dreamstime.com/b/%D0%BF%D0%BB%D0%B0%D0%BA%D0%B0%D1%82-%D0%BF%D0%B0%D1%80%D1%82%D0%B8%D0%B8-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D1%8B-%D1%8D%D0%BB%D0%B5%D0%B3%D0%B0%D0%BD%D1%82%D0%BD%D0%B0%D1%8F-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D0%B0-%D1%82%D0%B0%D0%BD%D1%86%D0%B5%D0%B2-%D0%BF%D0%B0%D1%80-%D1%80%D0%B5%D1%82%D1%80%D0%BE-%D1%82%D0%B8%D0%BF-%D0%B8-106201773.jpg",
-                name: "Salsa"),
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%BF%D0%BB%D0%B0%D0%BA%D0%B0%D1%82-%D0%BF%D0%B0%D1%80%D1%82%D0%B8%D0%B8-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D1%8B-%D1%8D%D0%BB%D0%B5%D0%B3%D0%B0%D0%BD%D1%82%D0%BD%D0%B0%D1%8F-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D0%B0-%D1%82%D0%B0%D0%BD%D1%86%D0%B5%D0%B2-%D0%BF%D0%B0%D1%80-%D1%80%D0%B5%D1%82%D1%80%D0%BE-%D1%82%D0%B8%D0%BF-%D0%B8-106201773.jpg",
+              name: "Salsa",
+            ),
             DanceStyleWidget(
-                url: "https://st2.depositphotos.com/8009420/10889/v/600/depositphotos_108892994-stock-illustration-couple-dancing-kizomba.jpg"
-                , name: "Kizomba")
+              url:
+                  "https://st2.depositphotos.com/8009420/10889/v/600/depositphotos_108892994-stock-illustration-couple-dancing-kizomba.jpg",
+              name: "Kizomba",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%B6%D0%B8%D0%B2%D1%8F-bachata-%D0%BC%D1%83%D0%B7%D1%8B%D0%BA%D0%B0-%D0%BC%D0%BE-%D0%BE-%D1%8B%D1%85-%D0%BF%D0%B0%D1%80-%D1%82%D0%B0%D0%BD%D1%86%D1%83%D1%8F-%D0%B0%D1%82%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-78010801.jpg",
+              name: "Bachata",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%BF%D0%BB%D0%B0%D0%BA%D0%B0%D1%82-%D0%BF%D0%B0%D1%80%D1%82%D0%B8%D0%B8-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D1%8B-%D1%8D%D0%BB%D0%B5%D0%B3%D0%B0%D0%BD%D1%82%D0%BD%D0%B0%D1%8F-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D0%B0-%D1%82%D0%B0%D0%BD%D1%86%D0%B5%D0%B2-%D0%BF%D0%B0%D1%80-%D1%80%D0%B5%D1%82%D1%80%D0%BE-%D1%82%D0%B8%D0%BF-%D0%B8-106201773.jpg",
+              name: "Salsa",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://st2.depositphotos.com/8009420/10889/v/600/depositphotos_108892994-stock-illustration-couple-dancing-kizomba.jpg",
+              name: "Kizomba",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%B6%D0%B8%D0%B2%D1%8F-bachata-%D0%BC%D1%83%D0%B7%D1%8B%D0%BA%D0%B0-%D0%BC%D0%BE-%D0%BE-%D1%8B%D1%85-%D0%BF%D0%B0%D1%80-%D1%82%D0%B0%D0%BD%D1%86%D1%83%D1%8F-%D0%B0%D1%82%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-78010801.jpg",
+              name: "Bachata",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%BF%D0%BB%D0%B0%D0%BA%D0%B0%D1%82-%D0%BF%D0%B0%D1%80%D1%82%D0%B8%D0%B8-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D1%8B-%D1%8D%D0%BB%D0%B5%D0%B3%D0%B0%D0%BD%D1%82%D0%BD%D0%B0%D1%8F-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D0%B0-%D1%82%D0%B0%D0%BD%D1%86%D0%B5%D0%B2-%D0%BF%D0%B0%D1%80-%D1%80%D0%B5%D1%82%D1%80%D0%BE-%D1%82%D0%B8%D0%BF-%D0%B8-106201773.jpg",
+              name: "Salsa",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://st2.depositphotos.com/8009420/10889/v/600/depositphotos_108892994-stock-illustration-couple-dancing-kizomba.jpg",
+              name: "Kizomba",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%B6%D0%B8%D0%B2%D1%8F-bachata-%D0%BC%D1%83%D0%B7%D1%8B%D0%BA%D0%B0-%D0%BC%D0%BE-%D0%BE-%D1%8B%D1%85-%D0%BF%D0%B0%D1%80-%D1%82%D0%B0%D0%BD%D1%86%D1%83%D1%8F-%D0%B0%D1%82%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F-78010801.jpg",
+              name: "Bachata",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://thumbs.dreamstime.com/b/%D0%BF%D0%BB%D0%B0%D0%BA%D0%B0%D1%82-%D0%BF%D0%B0%D1%80%D1%82%D0%B8%D0%B8-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D1%8B-%D1%8D%D0%BB%D0%B5%D0%B3%D0%B0%D0%BD%D1%82%D0%BD%D0%B0%D1%8F-%D1%81%D0%B0%D0%BB%D1%8C%D1%81%D0%B0-%D1%82%D0%B0%D0%BD%D1%86%D0%B5%D0%B2-%D0%BF%D0%B0%D1%80-%D1%80%D0%B5%D1%82%D1%80%D0%BE-%D1%82%D0%B8%D0%BF-%D0%B8-106201773.jpg",
+              name: "Salsa",
+            ),
+            DanceStyleWidget(
+              url:
+                  "https://st2.depositphotos.com/8009420/10889/v/600/depositphotos_108892994-stock-illustration-couple-dancing-kizomba.jpg",
+              name: "Kizomba",
+            ),
           ],
         ),
       ),
@@ -53,39 +101,6 @@ class DanceStyleWidget extends StatelessWidget {
       children: [
         Image.network(
           url,
-          width: 70,
-          height: 70,
-          fit: BoxFit.cover,
-        ),
-        const SizedBox(
-          width: 10,
-          height: 100,
-        ),
-        Text(name),
-      ],
-    );
-  }
-
-  Widget buildSecond(BuildContext context) {
-    return Row(children: [
-      Image.network(
-        url,
-        width: 70,
-        height: 70,
-        fit: BoxFit.cover,
-      ),
-      const SizedBox(
-        width: 10,
-        height: 100,
-      ),
-      Text(name),
-    ]);
-  }
-
-  Widget buildThird(BuildContext context) {
-    return Row(
-      children: [
-        Image.network(url,
           width: 70,
           height: 70,
           fit: BoxFit.cover,
